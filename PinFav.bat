1bf4aef1d65e020f2a5f0531bc66c736553788bc
IB_U_Z_Z_A_R_Dl <62464560+Illegal-Services@users.noreply.github.com>
2022-01-29 16:14:00 +0000

    Add files via upload

--- PinFav.bat
+++ PinFav.bat
@@ -1,6 +1,6 @@
 ::------------------------------------------------------------------------------
 :: NAME
-::     PinFav.bat
+::     PinFav.bat - Pin Favorite
 ::
 :: DESCRIPTION
 ::     Pin your favorite window.
